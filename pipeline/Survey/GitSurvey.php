6b806508cd9a7fe6d11a68d9de15d32bb52bf02f
Daniel Leech <dan.t.leech@gmail.com>
2021-01-03 11:21:08 +0000

    Hightlight repos to be tagged

--- pipeline/Survey/GitSurvey.php
+++ pipeline/Survey/GitSurvey.php
@@ -10,9 +10,10 @@ use Maestro\Core\Pipeline\Pipeline;
 use Maestro\Core\Report\TaskReportPublisher;
 use Maestro\Core\Task\ClosureTask;
 use Maestro\Core\Task\Context;
-use Maestro\Core\Task\GitSurveyTask;
 use Maestro\Core\Task\SequentialTask;
 use Maestro\Core\Task\Task;
+use Maestro\Git\Fact\GitSurveyFact;
+use Maestro\Git\Task\GitSurveyTask;
 use PhpactorHub\Pipeline\BasePipeline;
 
 class GitSurvey extends BasePipeline
@@ -21,17 +22,25 @@ class GitSurvey extends BasePipeline
     {
         return new SequentialTask([
             new ComposerTask(),
+            new GitSurveyTask(),
             new ClosureTask(function (Context $context) use ($repository) {
                 $composer = $context->fact(ComposerFact::class);
+                $git = $context->fact(GitSurveyFact::class);
+                assert($git instanceof GitSurveyFact);
                 assert($composer instanceof ComposerFact);
+
                 $context->service(TaskReportPublisher::class)->publishTableRow([
                     'alias' => implode(',', $context->fact(ComposerFact::class)->json()->branchAliases()),
-                    'next' => $repository->vars()->get('version')
+                    'next' => (function (string $version) use ($git) {
+                        $decoration = $version !== $git->latestTag() ? 'fg=yellow' : 'fg=white';
+                        return sprintf(
+                            '<%s>%s</>', $decoration, $version,
+                        );
+                    })($repository->vars()->get('version'))
                 ]);
 
                 return $context;
             }),
-            new GitSurveyTask()
         ]);
     }
 }
